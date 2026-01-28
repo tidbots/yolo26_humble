31f4020849a01ad2f2213d13c277a2f4123b3268
Hiroyuki Okada <hiroyuki.okd@icloud.com>
2026-01-28 15:15:44 +0900

    Update setup.py

--- ros2_ws/src/yolo26_ros2/setup.py
+++ ros2_ws/src/yolo26_ros2/setup.py
@@ -1,1 +1,27 @@
 
+from setuptools import setup
+
+package_name = "yolo26_ros2"
+
+setup(
+    name=package_name,
+    version="0.1.0",
+    packages=[package_name],
+    data_files=[
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
+        ("share/" + package_name + "/launch", ["launch/yolo26.launch.py"]),
+        ("share/" + package_name + "/config", ["config/yolo26.yaml"]),
+    ],
+    install_requires=["setuptools"],
+    zip_safe=True,
+    maintainer="tidbots",
+    maintainer_email="you@example.com",
+    description="YOLO26 object detector node for ROS 2 Humble (rclpy).",
+    license="Apache-2.0",
+    entry_points={
+        "console_scripts": [
+            "yolo26_node = yolo26_ros2.yolo26_node:main",
+        ],
+    },
+)
